c9190b3b2f129b2bf9ad3e390fbb254454d6fab6
Maor <60894008+MaorIdi@users.noreply.github.com>
2026-01-26 20:51:52 +0200

    C

--- cron_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This script runs the cleaner every 1 second using the python script's internal loop
-echo "Starting Mullvad Device Cleaner..."
-exec python3 -u mullvad_device_cleaner.py --apply --interval 1
